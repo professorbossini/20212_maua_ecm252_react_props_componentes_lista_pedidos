b5c3ae370c5e3a0c97a21263ecc50cc834d8063c
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-21 11:26:19 -0300

    Intervalo. FIzemos cartao e Pedido. Falta Feedback

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import Pedido from './Pedido'
+import Cartao from './Cartao'
+
+
+export const App = () => {
+    return (
+        <div className="container border rounded mt-2">
+            <div className="row border-bottom m-2">
+                <h1 className="display-5 text-center">Seus pedidos</h1>
+            </div>
+
+            <div className="row">
+                <div className="col-sm-8 col-md-6 m-2">
+                    <Cartao cabecalho="22/04/2021">
+                        <Pedido 
+                            icone="fas fa-hdd fa-2x" 
+                            titulo="SSD"
+                            descricao="SSD Kingston A400 - SATA"/>
+                    </Cartao>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm-8 col-md-6 m-2">
+                    <Cartao cabecalho="20/04/2021">
+                        <Pedido 
+                            icone="fas fa-book fa-2x" 
+                            titulo="Livro"
+                            descricao="Concrete Mathematics - Donald Knuth"/>
+                    </Cartao>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm-8 col-md-6 m-2">
+                <Cartao cabecalho="21/02/2021">
+                    <Pedido 
+                            icone="fas fa-laptop fa-2x" 
+                            titulo="Notebook"
+                            descricao="Notebook Dell - 8Gb - i5"/>
+                </Cartao>
+                </div>
+            </div>
+
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector("#root")
+)
